8b579eb30d4791c11f8a3ecb3c83fb438eb6d8e0
Aleksandr <smirnov9323@gmail.com>
2022-07-12 00:53:23 +0300

    edit

--- src/components/listUsers.vue
+++ src/components/listUsers.vue
@@ -3,14 +3,24 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-lg-5">
-          <hr>
+          <hr />
           <div class="input-group mb-3">
-            <input type="text" class="form-control"
-                   v-model="carentName"
-                   v-bind:placeholder="placeholder"
-                   aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <input
+              type="text"
+              class="form-control"
+              v-model="carentName"
+              v-bind:placeholder="placeholder"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
             <button type="button" class="btn btn-outline-primary">edit</button>
-            <button type="button" class="btn btn-outline-danger" v-on:click="removeUser(index)">delete</button>
+            <button
+              type="button"
+              class="btn btn-outline-danger"
+              v-on:click="removeUser(index)"
+            >
+              delete
+            </button>
           </div>
         </div>
       </div>
@@ -20,28 +30,26 @@
 
 <script>
 export default {
-  name: 'listUsers',
-  props: ['placeholder', 'name'],
+  name: "listUsers",
+  props: ["placeholder", "name"],
   data() {
     return {
-      input: '',
-      carentName: '',
+      input: "",
+      carentName: "",
       index: {
         type: Number,
-      }
-    }
+      },
+    };
   },
 
   methods: {
     removeUser(index) {
-      this.$emit('remove-user', index);
-    }
+      this.$emit("remove-user", index);
+    },
   },
 
   created() {
-    this.carentName = this.name
+    this.carentName = this.name;
   },
-
-
-}
+};
 </script>
