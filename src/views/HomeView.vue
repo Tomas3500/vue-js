b9e50c2342d7bef2f5903a85b2715abce286d302
Aleksandr <smirnov9323@gmail.com>
2022-07-12 00:52:00 +0300

    addUsers, removeUser

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <add-user placeholder="add user" @add-user="addUser"></add-user>
+    <list-users
+      v-for="(item, index) in users"
+      :key="item.id"
+      :remove-user="index"
+      :name="item.name"
+      @remove-user="removeUser"
+    >
+    </list-users>
+    <h2 v-if="users.length === 0">no users</h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import addUser from "@/components/addUser.vue";
+import listUsers from "@/components/listUsers.vue";
 
 export default {
-  name: 'HomeView',
+  name: "addUsers",
   components: {
-    HelloWorld
-  }
-}
+    addUser,
+    listUsers,
+  },
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Alex",
+          age: 20,
+        },
+        {
+          id: 2,
+          name: "Vasya",
+          age: 23,
+        },
+        {
+          id: 3,
+          name: "Petya",
+          age: 41,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addUser(event) {
+      this.users.unshift({
+        id: this.id,
+        name: event.item,
+      });
+    },
+    removeUser(event) {
+      let index = this.users.findIndex((index) => index === event);
+      this.users.splice(index, 1);
+    },
+  },
+};
 </script>
